abe9e6aaf11a284a6967eb408fff5b28408b12de
Faaizz <fr33ziey@gmail.com>
2022-04-25 00:43:12 +0200

    [T-1] Dynamically generate links for locales

--- schwarz_imprint/src/Controller/MainController.php
+++ schwarz_imprint/src/Controller/MainController.php
@@ -6,16 +6,31 @@ use App\Repository\PortalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MainController extends AbstractController
 {
-    #[Route('/{_locale}/imprint', name: 'app_imprint', requirements: ['_locale' => 'en|de'])]
-    public function imprint(string $_locale, PortalRepository $portalRepo): Response
+    #[Route('/{_locale}/{imprint?imprint}', name: 'app_imprint')]
+    public function imprint(string $_locale, string $imprint, PortalRepository $portalRepo): Response
     {
-        $portal = $portalRepo->findOneBy(['countryCode' => strtolower($_locale)]);
-        return $this->render('imprint/index.html.twig', [
+        $portal = $portalRepo->findOneBy(['countryCode' => strtolower($_locale), 'imprintLink' => strtolower($imprint)]);
+        return $this->render('imprint/view.html.twig', [
             'portal' => $portal,
         ]);
     }
 
+    #[Route('/', name: 'list_imprint')]
+    public function default(PortalRepository $portalRepo): Response
+    {
+        $portals = $portalRepo->findAll();
+        $imprintLinks = array_map( fn($portal) => $this->generateUrl('app_imprint', [
+                '_locale' => $portal->getCountryCode(),
+                'imprint' => $portal->getImprintLink(),
+            ] 
+        ), $portals);
+
+        return $this->render('imprint/index.html.twig', [
+            'imprintLinks' => $imprintLinks,
+        ]);
+    }
 }
