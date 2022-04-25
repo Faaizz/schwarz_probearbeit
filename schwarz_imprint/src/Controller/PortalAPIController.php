0eeaebff89cc6f88ce4f91ff65697e15d368653f
Faaizz <fr33ziey@gmail.com>
2022-04-25 02:30:02 +0200

    [T-2] Add REST API Controller

---
+++ schwarz_imprint/src/Controller/PortalAPIController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Business\PortalLogic;
+use App\Repository\PortalRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+#[Route('/api/v1')]
+class PortalAPIController extends AbstractController
+{
+    #[Route('/portal', methods: ['GET'])]
+    public function index(PortalLogic $portalLogic): Response
+    {
+        $portals = $portalLogic->index();
+
+        return $this->json([
+            'portals' => $portals,
+        ]);
+    }
+
+    #[Route('/portal', methods: ['POST'])]
+    public function create(Request $request, PortalLogic $portalLogic): Response
+    {
+        $requestParams = json_decode($request->getContent(), true);
+        $result = $portalLogic->create($requestParams['country_code'], $requestParams['imprint_link'], $requestParams['imprint']);
+
+        if ($result instanceof ConstraintViolationListInterface) {
+            return $this->json([
+                'error' => [
+                    'code' => 400,
+                    'title' => 'Validation Error',
+                    'message' => $result->get(0),
+                ],
+            ]);
+        }
+
+        return $this->json(['portal' => $result]);
+    }
+
+    #[Route('/portal/{id}', methods: ['PUT', 'PATCH'])]
+    public function update(Request $request, PortalRepository $portalRepository, PortalLogic $portalLogic, int $id): Response
+    {
+        $portal = $portalRepository->find($id);
+        if (!isset($portal)) {
+            return $this->json([
+                'error' => [
+                    'code' => 404,
+                    'title' => 'Not Found',
+                    'message' => 'Portal Not Found',
+                ],
+            ]);
+        }
+
+        $requestParams = json_decode($request->getContent(), true);
+        $portal = $portalLogic->update($id, $requestParams['imprint_link'], $requestParams['imprint']);
+
+        return $this->json(['portal' => $portal]);
+    }
+
+    #[Route('/portal/{id}', methods: ['DELETE'])]
+    public function delete(PortalLogic $portalLogic, int $id): Response
+    {
+        $portalLogic->delete($id);
+
+        return $this->json(['status' => 'success']);
+    }    
+}
