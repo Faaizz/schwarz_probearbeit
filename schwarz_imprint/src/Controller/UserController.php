4005d20c743296fa1537a8416ea3bc39526988c8
Faaizz <fr33ziey@gmail.com>
2022-04-26 15:21:08 +0200

    [T-3] Setup /user route, controller & logic

---
+++ schwarz_imprint/src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\Interfaces\UserServiceInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(UserServiceInterface $userService): Response
+    {
+        $users = $userService->getAll();
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
