d6963ecd80914c9708aff1699402230f2220de33
Faaizz <fr33ziey@gmail.com>
2022-04-27 09:30:45 +0200

    [T-5] Customize registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,42 +3,59 @@
 namespace App\Controller;
 
 use App\Entity\Login;
-use App\Form\RegistrationFormType;
+use App\Security\SecurityAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, ValidatorInterface $validator, UserAuthenticatorInterface $userAuthenticator, SecurityAuthenticator $securityAuthenticator): Response
     {
         $user = new Login();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-            $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+        $username = $request->request->get('username');
+        $plainPassword = $request->request->get('plainPassword');
 
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('index_imprint');
+        if (
+            !isset($username) ||
+            !isset($plainPassword)
+        ) {
+            return $this->render('registration/register.html.twig');
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
+        $user->setUsername(
+            $username
+        );
+
+        $user->setPassword(
+        $userPasswordHasher->hashPassword(
+                $user,
+                $plainPassword
+            )
+        );
+
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            return $this->render('registration/register.html.twig');
+        }
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        $userAuthenticator->authenticateUser(
+            $user,
+            $securityAuthenticator,
+            $request,
+        );
+
+        return $this->redirectToRoute('index_imprint');
+
     }
 }
